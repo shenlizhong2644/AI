0e0634a1191f5836bef0ea64660aa00bccf7df34
shenlizhong2644 <shenlizhong2644@gmail.com>
2015-01-13 17:22:58 +0800

    Remove useless interface
    Remove useless interface

--- BT/BTTest/BTTest/BehaviorTree/BevBaseNode.cs
+++ BT/BTTest/BTTest/BehaviorTree/BevBaseNode.cs
@@ -2,33 +2,35 @@
 
 namespace BehaviorTreeLib
 {
-		public class BevBaseNode :IBevNode
-		{
-				protected BevNodeType m_Type;
-				public BevCondition Condition;
-				private static long m_IDCount;
-				private long m_ID;
-                
-				public BevBaseNode ()
-				{
-						m_ID = m_IDCount++;
-				}
+    public class BevBaseNode
+    {
+        protected BevNodeType m_Type;
+        public BevCondition Condition;
+        private static long m_IDCount;
+        private long m_ID;
 
-				public virtual bool Tick ()
-				{
-						return true;
-				}
+        public BevBaseNode()
+        {
+            m_ID = m_IDCount++;
+        }
 
-				protected bool JudgeCondition ()
-				{
-						return Condition == null ? true : Condition.Judge ();
-				}
+        public virtual bool Tick()
+        {
+            return true;
+        }
 
-				public BevNodeType NodeType {
-						get {
-								return m_Type;
-						}
-				}
-			
-		}
+        protected bool JudgeCondition()
+        {
+            return Condition == null ? true : Condition.Judge();
+        }
+
+        public BevNodeType NodeType
+        {
+            get
+            {
+                return m_Type;
+            }
+        }
+
+    }
 }
--- BT/BTTest/BTTest/BehaviorTree/BevParallel.cs
+++ BT/BTTest/BTTest/BehaviorTree/BevParallel.cs
@@ -3,41 +3,43 @@ using System.Collections.Generic;
 
 namespace BehaviorTreeLib
 {
-		public class BevParallel :BevBaseNode
-		{
-				private List<BevBaseNode> m_Childs;
-				private BevNodeType m_Type;
+    public class BevParallel : BevBaseNode
+    {
+        private List<BevBaseNode> m_Childs;
+        private BevNodeType m_Type;
 
-				public BevParallel ():base()
-				{
-						m_Childs = new List<BevBaseNode> ();
-						m_Type = BevNodeType.BEV_NODE_PARALLEL;
-				}
-				
-				public void AddChild (BevBaseNode child)
-				{
-						m_Childs.Add (child);		
-				}
+        public BevParallel()
+            : base()
+        {
+            m_Childs = new List<BevBaseNode>();
+            m_Type = BevNodeType.BEV_NODE_PARALLEL;
+        }
 
-				public void AddChilds (params BevBaseNode[] childs)
-				{
-						m_Childs.AddRange (childs);
-				}
+        public void AddChild(BevBaseNode child)
+        {
+            m_Childs.Add(child);
+        }
 
-				public void RemoveChild (BevBaseNode child)
-				{
-						m_Childs.Remove (child);		
-				}
+        public void AddChilds(params BevBaseNode[] childs)
+        {
+            m_Childs.AddRange(childs);
+        }
 
-				public override bool Tick ()
-				{
-						if (!JudgeCondition ())
-								return false;
-						foreach (IBevNode child in m_Childs) {
-								child.Tick ();	
-						}
-						return true;
-				}
+        public void RemoveChild(BevBaseNode child)
+        {
+            m_Childs.Remove(child);
+        }
 
-		}
+        public override bool Tick()
+        {
+            if (!JudgeCondition())
+                return false;
+            foreach (BevBaseNode child in m_Childs)
+            {
+                child.Tick();
+            }
+            return true;
+        }
+
+    }
 }
